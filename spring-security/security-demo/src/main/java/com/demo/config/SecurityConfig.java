de47c0573ab3af581df61c101aa03448b6df4565
jadhAv2808 <pbj8007@gmail.com>
2023-06-17 23:46:51 +0530

    jwt added

--- spring-security/security-demo/src/main/java/com/demo/config/SecurityConfig.java
+++ spring-security/security-demo/src/main/java/com/demo/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.demo.config;
 
+import com.demo.filter.JwtAuthFilter;
 import com.demo.service.UserInfoUserDetailsService;
 import com.mysql.cj.protocol.AuthenticationProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,12 +22,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthFilter jwtAuthFilter;
 
     //bean to define user details----------authentication
     @Bean
@@ -61,14 +69,18 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
        return httpSecurity.csrf().disable().authorizeHttpRequests()
-                .requestMatchers("/home/public","/users/addUser")
+                .requestMatchers("/home/public","/users/addUser","/jwt/authenticate")
                 .permitAll()
                 .and().authorizeHttpRequests()
                 .requestMatchers("/home/**")
                 .authenticated()
                 .and()
-                .formLogin()
+                //.formLogin()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .authenticationProvider(authenticationProvider())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class )
                 .build();
 
 
@@ -83,6 +95,12 @@ public class SecurityConfig {
         authenticationProvider.setUserDetailsService(userDetailsService());
         authenticationProvider.setPasswordEncoder(passwordEncoder());
         return authenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 
 }
